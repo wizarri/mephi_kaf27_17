d5c397fda5ad24c7cc5f83035ae2f80d95457e6c
alex-belyakov <32854858+alex-belyakov@users.noreply.github.com>
2017-11-01 19:46:37 +0300

    Add files via upload

--- generator_part.v
+++ generator_part.v
@@ -12,24 +12,31 @@ module generator_part
   output [31:0] out
 );
 
-  reg [31:0] reg_s;
-  wire [31:0] wire_s, shift_l1, bit_and, xor1, shift_r, shift_l2, xor2;
+  reg [31:0] reg_s, reg_l, reg_r;
+  wire [31:0] wire_s, wire_l, wire_r, shift_l1, bit_and, xor1, shift_r, shift_l2, xor2;
   
   always @(posedge clk or posedge rst) 
     begin
-  	  if (rst)
+  	  if (rst) begin
   		  reg_s <= SEED;
+		  reg_l <= 32'h0;
+		  reg_r <= 32'h0;
+	  end  
   	  else begin
   		  reg_s <= xor2;
+		  reg_l <= xor1;
+		  reg_r <= bit_and;
   	  end
     end
-
+	 
   assign wire_s   = reg_s;
-  assign shift_l1 = wire_s << SHIFT_L1;
+  assign wire_r   = reg_r;
+  assign wire_l   = reg_l; 
+  assign shift_l1 = wire_s <<< SHIFT_L1;
   assign bit_and  = wire_s & CONST;
   assign xor1     = shift_l1 ^ wire_s;
-  assign shift_r  = xor1 >> SHIFT_R;
-  assign shift_l2 = bit_and << SHIFT_L2;
+  assign shift_r  = wire_r >>> SHIFT_R;
+  assign shift_l2 = wire_l <<< SHIFT_L2;
   assign xor2     = shift_r ^ shift_l2;
   assign out      = xor2;
   
