959545e2c1062d8e99555b6855a08deb71e132bd
YanglyaevRamil <32882454+YanglyaevRamil@users.noreply.github.com>
2017-11-13 17:25:18 +0300

    Add files via upload

--- rtl/memory/regAB.sv
+++ rtl/memory/regAB.sv
@@ -1,3 +1,4 @@
+`timescale 1ns / 1ps
 module rom_AxB
 	
 	#(parameter A=32, width=4, depth=16 )
@@ -11,17 +12,17 @@ module rom_AxB
 
 	function [width-1:0] fx;
 		input [depth-1:0] mem;	
-		begin
+		  begin
 			fx = 0; 
-			for (i = 0; i < depth; i=1+i)
-				fx = i; 	
+			for (i = 0; i<depth ; i=i+1)
+				fx[i] = i; 	
 		end
 	endfunction 
 	
 	initial begin
 		for (i = 0; 0 < depth; i=i+1) 
 			begin
-				memory[i] = fx; 
+				memory[i] = fx[i]; 
 			end
 	end
 endmodule
