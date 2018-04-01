cb3e4e91ba87f28202da7a19de3c9633ffa7ef2c
alex-belyakov <32854858+alex-belyakov@users.noreply.github.com>
2018-04-02 00:57:37 +0300

    Add files via upload

--- rtl/Box-Muller/box_muller.v
+++ rtl/Box-Muller/box_muller.v
@@ -1,149 +1,101 @@
- module box_muller
+module sborka
 #(
-	parameter 	OUT = 32, // Разрядность выходных данных 32 или 48
-			  	FILE_SQRT = "sqrt.txt", //Файл со значениями корня 
-			  	FILE_SIN  = "sin.txt", //Файл со значениями синуса
-			  	FILE_COS  = "cos.txt", //Файл со значениями косинуса
-			  	A_SQRT = 24, //Разрядность адреса к Square root and Logarithm Unit
-			  	D_SQRT = 24, //Разрядность данных из Square root and Logarithm Unit
-			  	A_CSIN = 16, //Разрядность адреса к Sin/Cos Unit (учитывается знаковый бит)
-			  	D_CSIN = 16, //Разрядность данных к Sin/Cos Unit
-			  	//Задавамые параметры для 1-ого генератора Tausworthe
-			  	SEED1_1      = 32'hE761B9DB,
-           	  	CONST1_1     = 32'hfffffffe,
-              	SEED2_1      = 32'hB4B4D15C,
-              	CONST2_1     = 32'hfffffff8,
-           		SEED3_1      = 32'hC0B4DD55,
-          		CONST3_1     = 32'hfffffff0,
-           		//Задавамые параметры для 2-ого генератора Tausworthe (желательно поменять)
-			 	SEED1_2      = 32'hE761B9DB,
-           		CONST1_2     = 32'hfffffffe,
-           		SEED2_2      = 32'hB4B4D15C,
-           		CONST2_2     = 32'hfffffff8,
-           		SEED3_2      = 32'hC0B4DD55,
-           		CONST3_2     = 32'hfffffff0
-
+	parameter SEED1  = 32'hE761B9DB, //Задаваемые 
+              CONST1 = 32'hfffffffe, //значения
+              SEED2  = 32'hB4B4D15C, //для
+              CONST2 = 32'hfffffff8, //Tausworthe
+              SEED3  = 32'hC0B4DD55,
+              CONST3 = 32'hfffffff0,
+              //---------SQRT UNIT-----------------------
+              A_SQRT    = 24, //Разрядность адреса
+              D_SQRT    = 24, //Разрядность данных
+              FILE_SQRT = "sqrt.txt", //Файл со значениями (в данном
+              							//случае 3 старших бита - целая часть
+              //------COS and SIN UNIT------------------
+              A_CSIN   = 16, //Разрядность адреса
+              D_CSIN   = 16, //Разрядность данных
+              FILE_COS = "cos.txt", //Файлы со значениями (в данном
+              FILE_SIN = "sin.txt",  //случае старший бит - знак числа)
+              //------------MULT-----------------------
+              N1    = 24, //Разрядность 1 числа (sqrt)
+              N2    = 16, //Разрядность 2 числа (sin or cos)
+              N_RES = 32  //Разрядность выходного значения (sqrt * sin or cos)
 )
 (
 	input clk, rst,
-	output [OUT-1:0] out1, out2 //out1 - умножение на син, out2 - на косин
+	output [D_SQRT-1:0] data_sqrt, //Выходные значения из SQRT UNIT
+	output [D_CSIN-1:0] data_cos, data_sin, //Выходные значения из COS and SIN UNIT
+	output [N_RES-1:0] out_cos, out_sin //Выходные значения
 );
 
-//Промежуточные сигналы
-	wire [31:0] a, b; //Выходные значения из генераторов Tausworthe
-	//wire [A_SQRT-1:0] u1;   //к Square root and Logarithm Unit
-	//wire [A_CSIN:0] u2;   //к Sin/Cos 
-	wire out_val_s1, out_val_s2;    //Сигналы с промежуточных регистров в 
-	wire out_val_lr1, out_val_lr2;  //генераторе Tausworthe 
-	wire re_s, re_lr;          //Сигналы разрешения чтения
-	wire [A_SQRT-1:0] adr_sqrt; //Адрес к Square root and Logarithm Unit
-	wire [D_SQRT-1:0] data_sqrt;    //Данные из Square root and Logarithm Unit
-	wire [A_CSIN:0] adr_csin; //Адрес к Sin/Cos Unit
-	wire [D_CSIN-1:0] data_sin, data_cos; //Данные из Sin/Cos Unit
-	//wire [D_SQRT-1:0] mn1_sin, mn1_cos;   //Множители при умножении на синус и 
-	//wire [D_CSIN-1:0] mn2_sin, mn2_cos;   //косинус 
+	wire [31:0] generator_out;
 
-//Подключение генераторов Tausworthe
-	tausworthe
-	#(
-		.SEED1(SEED1_1), .CONST1(CONST1_1), .SEED2(SEED2_1), .CONST2(CONST2_1),
-		.SEED3(SEED3_1), .CONST3(CONST3_1)
-	)
-	number1
+//-----------------------Tauswothe URNG-------------------
+	tausworthe #(.SEED1(SEED1), .CONST1(CONST1), .SEED2(SEED2), 
+		.CONST2(CONST2), .SEED3(SEED3), .CONST3(CONST3))
+
+	tausworthe1
 	(
 		.clk(clk),
-    	.rst(rst),
-    	.generator_out(a),
-    	.out_val_s(out_val_s1), 
-    	.out_val_lr(out_val_lr1)
+		.rst(rst),
+		.generator_out(generator_out),
+		.out_val_s(out_val_s),
+		.out_val_lr(out_val_lr)
 	);
 
-	tausworthe
-	#(
-		.SEED1(SEED1_2), .CONST1(CONST1_2), .SEED2(SEED2_2), .CONST2(CONST2_2),
-		.SEED3(SEED3_2), .CONST3(CONST3_2)
-	)
-	number2
-	(
-		.clk(clk),
-    	.rst(rst),
-    	.generator_out(b),
-    	.out_val_s(out_val_s2), 
-    	.out_val_lr(out_val_lr2)
-	);
+//-------------------------SQRT UNIT---------------------
+	sqrt_rom #(.A_WIDTH(A_SQRT), .D_WIDTH(D_SQRT), .FILE(FILE_SQRT))
 
-//Разделение сигналов
-	assign adr_sqrt = a[A_SQRT-1:0]; 
-	assign adr_csin = b[A_CSIN:0];
-
-//Подключение блоков памяти 
-    //Square root and Logarithm Unit 
-	sqrt_rom 
-	#(
-		.A_WIDTH(A_SQRT), .D_WIDTH(D_SQRT), .FILE(FILE_SQRT)
-	)
 	sqrt_unit
 	(
 		.clk(clk),
-		.adress(adr_sqrt),
+		.adress(generator_out[A_SQRT-1:0]),
 		.data(data_sqrt),
-		.re_lr(out_val_lr1),
-		.re_s(out_val_s1)
+		.re_s(out_val_s)
 	);
 
-    //Sin Unit
-	csin_rom 
-	#(
-		.A_WIDTH(A_CSIN), .D_WIDTH(D_CSIN), .FILE(FILE_SIN)
-	)
-	sin_unit
-	(
-		.clk(clk),
-		.adress(adr_csin),
-		.data(data_sin),
-		.re_lr(out_val_lr2),
-		.re_s(out_val_s2)
-	);
+//-------------------------COS UNIT-----------------------
+	cos_mem #(.A_WIDTH(A_CSIN), .D_WIDTH(D_CSIN), .FILE(FILE_COS))
 
-	//Cos Unit
-	csin_rom 
-	#(
-		.A_WIDTH(A_CSIN), .D_WIDTH(A_CSIN), .FILE(FILE_COS)
-	)
 	cos_unit
 	(
 		.clk(clk),
-		.adress(adr_csin),
+		.adress(generator_out[A_CSIN-1:0]),
 		.data(data_cos),
-		.re_lr(out_val_lr2),
-		.re_s(out_val_s2)
+		.re_s(out_val_s)
 	);
 
-//Подключение умножителей
-	//Умножение на sin
-	mult
-	#(
-		.N1(D_SQRT), .N2(D_CSIN), .N_RES(OUT) 
-	)
-	mult_sin
+//-------------------------SIN UNIT-----------------------
+	sin_mem #(.A_WIDTH(A_CSIN), .D_WIDTH(D_CSIN), .FILE(FILE_SIN))
+
+	sin_unit
 	(
 		.clk(clk),
-		.mn1(data_sqrt),
-		.mn2(data_sin),
-		.result(out1)
+		.adress(generator_out[A_CSIN:0]),
+		.data(data_sin),
+		.re_s(out_val_s)
 	);
 
-	//Умножение на cos
-	mult
-	#(
-		.N1(D_SQRT), .N2(D_CSIN), .N_RES(OUT) 
-	)
+//-------------------------MULT COS-----------------------
+	mult #(.N1(N1), .N2(N2), .N_RES(N_RES))
+
 	mult_cos
 	(
 		.clk(clk),
 		.mn1(data_sqrt),
 		.mn2(data_cos),
-		.result(out2)
+		.result(out_cos)
 	);
 
-endmodule // box_muller
+//-------------------------MULT SIN-----------------------
+	mult #(.N1(N1), .N2(N2), .N_RES(N_RES))
+
+	mult_sin
+	(
+		.clk(clk),
+		.mn1(data_sqrt),
+		.mn2(data_sin),
+		.result(out_sin)
+	);
+
+endmodule
